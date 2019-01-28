f4d66f34af1f2f2c888fe84d86d8b5143a815d60
clay <1150689758@qq.com>
2019-01-28 15:03:31 +0800

    button 基本样式

--- src/app.js
+++ src/app.js
@@ -7,5 +7,12 @@ new Vue({
   el: '#app',
   data: {
 
-  }
+  },
+  template: 
+  `
+  <div>
+    <g-button icon="setting">按钮</g-button>
+    <g-button icon="setting">按钮</g-button>
+  </div>
+  `
 })
--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
-    <button>
-      <g-icon :name="icon"></g-icon>
-      <slot></slot>
-    </button>
-  </div>
+  <button class="g-button">
+    <g-icon :name="icon"></g-icon>
+    <slot></slot>
+  </button>
 </template>
 <script>
   import Icon from './icon'
@@ -19,4 +17,31 @@
   }
 </script>
 <style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  .g-button {
+    background: var(--buttonBg);
+    color: var(--color);
+    border: 1px solid var(--borderColor);
+    border-radius: .3em;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5em 1.165em;
+    & .g-icon {
+      margin-right: 0.4em;
+    }
+    &:hover {
+      border-color: var(--borderColorHover);
+    }
+    &:focus {
+      outline: none;
+    }
+    &:active {
+      background: var(--buttonActiveBg);
+    }
+  }
 </style>
