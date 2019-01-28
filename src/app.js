c9b547ef76f5ead05631c995dc7aa4fcd0e7e75a
clay <1150689758@qq.com>
2019-01-28 19:15:59 +0800

    button 单元测试

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ new Vue({
   `
   <div>
     <g-button icon="setting" @click="handleClick">按钮</g-button>
-    <g-button icon="setting" :icon-only="true"></g-button>
+    <g-button icon="setting" :icon-only="true" :loading="true"></g-button>
     <g-button icon="setting">按钮</g-button>
     <g-button icon="download" icon-position="right">按钮</g-button>
     <g-button-group>
@@ -28,3 +28,87 @@ new Vue({
   </div>
   `
 })
+
+import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+
+const expect = chai.expect
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  vm.$mount()
+  let spy = chai.spy(function(){})
+
+  vm.$on('click', spy)
+  // 希望这个函数被执行
+  let button = vm.$el
+  button.click()
+  expect(spy).to.have.been.called()
+}
--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,10 @@
 <template>
-  <button :class="[{ iconOnly: iconOnly }, 'g-button']" @click="$emit('click')">
-    <g-icon :name="icon" v-if="icon" :class="[iconPosition]"></g-icon>
-    <slot></slot>
+  <button :class="[{ iconOnly: iconOnly }, [iconPosition], 'g-button']" @click="$emit('click')">
+    <g-icon :name="icon" v-if="icon && !loading" class="icon"></g-icon>
+    <g-icon class="loading" v-if="loading" name="loading"></g-icon>
+    <div class="gulu-button-content">
+      <slot/>
+    </div>
   </button>
 </template>
 <script>
@@ -23,6 +26,10 @@
         validator: function (value) {
           return value === 'left' || value === 'right'
         }
+      },
+      loading: {
+        type: Boolean,
+        default: false
       }
     }
   }
@@ -33,6 +40,14 @@
     padding: 0;
     box-sizing: border-box;
   }
+  @keyframes loading {
+    0% {
+      transform: rotate(0)
+    }
+    100% {
+      transform: rotate(360deg)
+    }
+  }
   .g-button {
     background: var(--buttonBg);
     color: var(--color);
@@ -42,6 +57,14 @@
     justify-content: center;
     align-items: center;
     padding: 0.5em 1.165em;
+    vertical-align: middle;
+    order: 0;
+    > .gulu-button-content {
+      order: 2;
+    }
+    > .icon {
+      order: 1;
+    }
     &.iconOnly {
       border-radius: 50%;
       padding: .64em;
@@ -51,11 +74,23 @@
     }
     & .g-icon {
       margin-right: 0.4em;
+      &.loading {
+        animation: loading 3s infinite linear; 
+      }
     }
-    & .g-icon.right {
+    &.right .icon {
+      order: 2;
+      &.g-icon {
+        margin-left: 0.4em;
+      }
+    }
+    &.right > .gulu-button-content{
+      order: 1;
+    }
+    /* &.left .g-icon {
       margin-left: 0.4em;
       order: 2;
-    }
+    } */
     &:hover {
       border-color: var(--borderColorHover);
     }
